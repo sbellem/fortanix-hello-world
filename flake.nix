6af2ea36af45dcf224fc7acec0885bb3dca3f844
Sylvain Bellemare <sbellem@gmail.com>
2022-03-02 01:38:32 -0600

    app, wip

--- flake.nix
+++ flake.nix
@@ -7,79 +7,96 @@
   inputs.fortanix-sgx-tools.url = "github:initc3/nix-fortanix?dir=fortanix-sgx-tools";
   inputs.sgxs-tools.url = "github:initc3/nix-fortanix?dir=sgxs-tools";
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, fortanix-sgx-tools, sgxs-tools }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    fortanix-sgx-tools,
+    sgxs-tools,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-      in with pkgs; {
-        defaultPackage = rustPlatform.buildRustPackage rec {
-          pname = "fortanix-hello-world";
-          version = "0.0.0";
+      in rec {
+          defaultPackage = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "fortanix-hello-world";
+            version = "0.0.0";
 
-          src = builtins.path { path = ./.; name = "${pname}-${version}"; };
+            src = builtins.path {
+              path = ./.;
+              name = "${pname}-${version}";
+            };
 
-          cargoSha256 = "sha256-oI59jnr2Dzkq4rO3+RL2v7f5wudXpSMESo5j8wRrCUY=";
+            cargoSha256 = "sha256-oI59jnr2Dzkq4rO3+RL2v7f5wudXpSMESo5j8wRrCUY=";
 
-          nativeBuildInputs = [
-            fortanix-sgx-tools.defaultPackage.${system}
-            (rust-bin.nightly."2021-12-05".default.override {
-              targets = [
-                "x86_64-fortanix-unknown-sgx"
-              ];
-            })
-          ];
+            nativeBuildInputs = with pkgs; [
+              fortanix-sgx-tools.defaultPackage.${system}
+              (rust-bin.nightly."2021-12-05".default.override {
+                targets = [
+                  "x86_64-fortanix-unknown-sgx"
+                ];
+              })
+            ];
 
-          buildInputs = [
-            openssl.dev
-          ];
+            buildInputs = with pkgs; [
+              openssl.dev
+            ];
 
-          buildPhase = ''
-            runHook preBuild
+            buildPhase = ''
+              runHook preBuild
 
-            cargo build --target x86_64-fortanix-unknown-sgx
+              cargo build --target x86_64-fortanix-unknown-sgx
 
-            ftxsgx-elf2sgxs target/x86_64-fortanix-unknown-sgx/debug/hello \
-              --heap-size 0x20000 \
-              --stack-size 0x20000 \
-              --debug
+              ftxsgx-elf2sgxs target/x86_64-fortanix-unknown-sgx/debug/hello \
+                --heap-size 0x20000 \
+                --stack-size 0x20000 \
+                --debug
 
-            runHook postBuild
-          '';
+              runHook postBuild
+            '';
 
-          installPhase = ''
-            runHook preInstall
+            installPhase = ''
+              runHook preInstall
 
-            mkdir -p $out/bin
-            cp -r target/x86_64-fortanix-unknown-sgx/debug $out/bin/debug
-            cp -r target/x86_64-fortanix-unknown-sgx/debug/hello.sgxs $out/bin/fortanix-hello-world
+              mkdir -p $out/bin
+              cp -r target/x86_64-fortanix-unknown-sgx/debug/hello.sgxs $out/bin/hello.sgxs
 
-            runHook postInstall
+              runHook postInstall
             '';
 
             doCheck = false;
-        };
+          };
 
-        #defaultApp = flake-utils.lib.mkApp {
-        #  drv = self.defaultPackage."${system}";
-        #} // {
-        #  CARGO_TARGET_X86_64_FORTANIX_UNKNOWN_SGX_RUNNER = "ftxsgx-runner-cargo";};
+          #defaultApp = apps.hello;
+          #apps.hello = {
+          #  type = "app";
+          #  program = "c-hello.packages.${system}.hello";
+          #};
 
-        devShell = mkShell {
-          CARGO_TARGET_X86_64_FORTANIX_UNKNOWN_SGX_RUNNER = "ftxsgx-runner-cargo";
-          buildInputs = [
-            fortanix-sgx-tools.defaultPackage.${system}
-            (rust-bin.nightly."2021-12-05".default.override {
-              targets = [
-                "x86_64-fortanix-unknown-sgx"
-              ];
-            })
-            sgxs-tools.defaultPackage.${system}
-          ];
-          packages = [ sgxs-tools ];
-        };
-      }
+          #defaultApp = {
+          #  type = "app";
+          #  #program = "${fortanix-sgx-tools}.defaultPackage.${system}/bin/ftxsgx-runner" self.defaultPackage/bin/hello.sgxs;
+          #  #program = "${fortanix-sgx-tools}.defaultPackage.${system}/bin/ftxsgx-runner --help";
+          #  program = "${pkgs}.hello";
+          #  #program = pkgs.hello;
+          #};
+
+          devShell = pkgs.mkShell {
+            CARGO_TARGET_X86_64_FORTANIX_UNKNOWN_SGX_RUNNER = "ftxsgx-runner-cargo";
+            buildInputs = with pkgs; [
+              fortanix-sgx-tools.defaultPackage.${system}
+              (rust-bin.nightly."2021-12-05".default.override {
+                targets = [
+                  "x86_64-fortanix-unknown-sgx"
+                ];
+              })
+              sgxs-tools.defaultPackage.${system}
+            ];
+          };
+        }
     );
 }
